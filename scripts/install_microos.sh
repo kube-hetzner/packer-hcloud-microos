1f048d7302870c561ce24de52b965aca72aedab8
Karim Naufal <rimkashox@gmail.com>
2023-01-12 18:23:37 +0100

    fix the disk not growing

--- scripts/install_microos.sh
+++ scripts/install_microos.sh
@@ -6,5 +6,12 @@ wget --timeout=5 https://ftp.gwdg.de/pub/opensuse/repositories/devel:/kubic:/ima
 # write image to internal disk /dev/sda
 qemu-img convert -p -f qcow2 -O host_device $(ls -a | grep -ie '^opensuse.*microos.*qcow2$') /dev/sda
 
+# grow the sda4 partition to the end of the disk
+parted -s /dev/sda resizepart 4 100%
+
+# sda4 is mounted to /var on the btfs filesystem, first mount it, then grow the filesystem
+mount /dev/sda4 /var
+btrfs filesystem resize max /var
+
 # reboot
 sleep 2; reboot
